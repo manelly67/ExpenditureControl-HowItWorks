a6ae107fa0326ac1430a8ff3439e6d9388200482
maria nelly lopez <manellylopezqqq@gmail.com>
2025-11-14 15:32:19 -0500

    after run build

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-"# webpack-template with jest" 
+## This page explain how the web app Coffee or Tea for controlling budget expenses works
+
+The link to use the app is https://coffeetea.netlify.app
--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
         type: 'asset/resource',
       },
       {
