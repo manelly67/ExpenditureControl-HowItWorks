0bcb634bf61a0be2542fa5283b50fcc08f60460c
maria nelly lopez <manellylopezqqq@gmail.com>
2025-11-14 19:07:25 -0500

    add favicon

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-import './styles/style-reset.css';
-import './styles/style.css';
+import "./styles/style-reset.css";
+import "./styles/style.css";
+import logo from "./assets/mn_bg_white.ico";
 import { googleFonts } from "./components/google-fonts";
 import { homepage } from "./components/homepage";
+
+const link = document.createElement("link");
+link.setAttribute("rel", "icon");
+link.setAttribute("type", "image/ico");
+link.setAttribute("href", logo);
 
 const meta = document.createElement("meta");
 meta.setAttribute("name", "description");
@@ -14,3 +20,4 @@ googleFonts();
 homepage();
 
 document.head.appendChild(meta);
+document.head.appendChild(link);
